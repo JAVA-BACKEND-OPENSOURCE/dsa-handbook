8441ac0065fbf4dd8169f99ab6200be82c06e980
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-09-09 15:13:12 +0700

    create singly linked list implementation

---
+++ linkedlist/singlylinkedlist/Node.java
@@ -0,0 +1,10 @@
+package singlylinkedlist;
+
+public class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
---
+++ linkedlist/singlylinkedlist/SinglyLinkedListImpl.java
@@ -0,0 +1,11 @@
+package singlylinkedlist;
+
+public class SinglyLinkedListImpl {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        
+    }
+}
