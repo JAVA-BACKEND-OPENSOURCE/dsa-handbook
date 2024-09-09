2cb3d44b84494eb88925ddd9e9b1eecf3b2fac9c
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-09-09 15:13:38 +0700

    create doubly linked list implementation

--- linkedlist/doublylinkedlist/Node.java
+++ linkedlist/doublylinkedlist/Node.java
@@ -1,4 +1,13 @@
 package doublylinkedlist;
 
 public class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
 }
